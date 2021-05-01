b9c0b123e6af2f8cf36d900b060ef595db1eb6a2
Steven Middaugh <smiddaugh47@gmail.com>
2021-05-01 13:10:44 -0700

    Workshop 5 finished

--- nucampsite/src/components/AboutComponent.js
+++ nucampsite/src/components/AboutComponent.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 function RenderPartner({partner}) {
     if (partner) {
         return(
             <React.Fragment>
-                <Media object src={partner.image} alt={partner.name} width="150"/>
+                <Media object src={baseUrl + partner.image} alt={partner.name} width="150"/>
                 <Media body className="ml-5 mb-4">
                     <Media heading >
                         {partner.name}
@@ -22,15 +24,30 @@ function RenderPartner({partner}) {
     }
 }
 
-function About(props) {
+function PartnerList(props) {
 
-    const partners = props.partners.map(partner => {
+    const partners = props.partners.partners.map(partner => {
         return (
             <Media tag="li" key={partner.id}>
                 <RenderPartner partner = {partner} />
             </Media>
         );
     });
+
+    if (props.partners.isLoading) {
+        return <Loading />;
+    }
+    if (props.partners.errMess) {
+        return <h4>{props.partners.errMess}</h4>;
+    }
+    return (
+        <div className="col mt-4">
+            <Media list > {partners} </Media>
+        </div>
+    )
+}
+
+function About(props) {
 
     return (
         <div className="container">
@@ -85,9 +102,7 @@ function About(props) {
                     <h3>Community Partners</h3>
                 </div>
                 <div className="col mt-4">
-                    <Media list>
-                        {partners}
-                    </Media>
+                    <PartnerList partners = {props.partners} />
                 </div>
             </div>
         </div>
